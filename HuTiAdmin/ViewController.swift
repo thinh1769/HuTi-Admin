da8536d1e918cbb6bbfb2b054b3d82d6ccb51276
Nguyễn Phúc Thịnh <thinh1769@gmail.com>
2023-04-29 17:21:41 +0700

    Initial Commit

---
+++ HuTiAdmin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HuTiAdmin
+//
+//  Created by Nguyễn Thịnh on 29/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
